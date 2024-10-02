cc4451308fe7e75726cf5629ec5e651ae0a189f3
Ammar-theScorpion <assiammar692@gmail.com>
2024-10-02 22:59:21 +0300

    cookiecutter init platform

---
+++ tutor/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from tutor.users.models import User
+from tutor.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
